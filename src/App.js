5f6cea62228073d64fde2240bbc0f0e6ca60774e
Fathurrachman D. Saputra <namasayafathur@gmail.com>
2022-05-28 12:17:44 +0700

    Adding edit profile

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from "./pages/login.jsx";
 import Reset from "./pages/Reset.jsx";
 import ResetPassword from "./pages/ResetPassword.jsx";
 import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/reset" element={<Reset />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/edit" element={<EditProfile />} />
         </Routes>
       </BrowserRouter>
     </div>
