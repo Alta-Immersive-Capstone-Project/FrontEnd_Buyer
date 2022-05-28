9a688c2f463e6fdf373dfe80aac69f99f5bfe398
Pringgo <macbookair@Macbooks-MacBook-Air.local>
2022-05-28 14:44:15 +0700

    Slicing Components Done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <div className="bgFooter">
+      <div className="backFooter">
+        <span className="spanFooter">About</span>
+        <span className="spanFooter">Contact Us</span>
+        <span className="spanFooter">Help</span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
