56b8f8127ce7c59647c336c5f5910e99132fae7f
Fathurrachman D. Saputra <namasayafathur@gmail.com>
2022-05-28 11:11:57 +0700

    Adding react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/login.jsx";
 import Reset from "./pages/Reset.jsx";
 import ResetPassword from "./pages/ResetPassword.jsx";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
-      {/* <Register />
-      <Login />
-      <Reset /> */}
-      <ResetPassword />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
