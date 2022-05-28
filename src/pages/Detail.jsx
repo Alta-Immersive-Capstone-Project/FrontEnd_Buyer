536ea213bfdcc9f5bb944c5518003c5736c686e8
fathonio <fathoniahmad1347@gmail.com>
2022-05-28 15:26:38 +0700

    progressbar

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { Card, Container, Form, Button, ProgressBar } from "react-bootstrap";
+import {
+  Card,
+  Container,
+  Form,
+  Button,
+  ProgressBar,
+  Row,
+  Col,
+} from "react-bootstrap";
 import "../styles/detail.css";
 
 import kost1 from "../images/kost1.svg";
-// import Star from "../../images/Star.svg";
+import Star from "../images/Star.svg";
 
 export default function Detail() {
   return (
@@ -81,29 +89,55 @@ export default function Detail() {
           </div>
 
           {/* baris 5 */}
-          <div>
+          <div className="text-start">
             <h4>Review</h4>
-            <h1>4.9</h1>
             <div>
-              <div>
-                <ProgressBar variant="success" now={98} />
-                <br />
-                <ProgressBar variant="success" now={95} />
-                <br />
-                <ProgressBar variant="success" now={20} />
-                <br />
-                <ProgressBar variant="success" now={10} />
-                <br />
-                <ProgressBar variant="success" now={5} />
-              </div>
-              <div className="py-3">
-                <h4>Anonim</h4>
-                <p>1 mount ago</p>
-                <p>
-                  Tempatnya bagus, lokasinya dekat dengan apapun. Penjaganya
-                  ramah. boleh bawa teman menginap.
-                </p>
-              </div>
+              <Row>
+                <Col className="text-center py-5" xs={3} md={2}>
+                  <h1>4.9</h1>
+                </Col>
+                <Col className="text-end" xs={3} md={2}>
+                  <div className="d-flex justify-content-end">
+                    <img className="imgStar" src={Star} alt="star" />
+                    <h4 className="ms-2">5</h4>
+                  </div>
+                  <div className="d-flex justify-content-end">
+                    <img className="imgStar" src={Star} alt="star" />
+                    <h4 className="ms-2">4</h4>
+                  </div>
+                  <div className="d-flex justify-content-end">
+                    <img className="imgStar" src={Star} alt="star" />
+                    <h4 className="ms-2">3</h4>
+                  </div>
+                  <div className="d-flex justify-content-end">
+                    <img className="imgStar" src={Star} alt="star" />
+                    <h4 className="ms-2">2</h4>
+                  </div>
+                  <div className="d-flex justify-content-end">
+                    <img className="imgStar" src={Star} alt="star" />
+                    <h4 className="ms-2">1</h4>
+                  </div>
+                </Col>
+                <Col xs={6} md={4}>
+                  <ProgressBar variant="success" now={98} />
+                  <br />
+                  <ProgressBar variant="success" now={95} />
+                  <br />
+                  <ProgressBar variant="success" now={10} />
+                  <br />
+                  <ProgressBar variant="success" now={5} />
+                  <br />
+                  <ProgressBar variant="success" now={3} />
+                </Col>
+                <Col xs={6} md={4}>
+                  <h4>Anonim</h4>
+                  <p>1 mount ago</p>
+                  <p>
+                    Tempatnya bagus, lokasinya dekat dengan apapun. Penjaganya
+                    ramah. boleh bawa teman menginap.
+                  </p>
+                </Col>
+              </Row>
             </div>
           </div>
         </Card>
---
+++ src/pages/Order.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Order() {
+  return (
+    <>
+      <div className="text-start">
+        <h1>Payment</h1>
+      </div>
+    </>
+  );
+}
